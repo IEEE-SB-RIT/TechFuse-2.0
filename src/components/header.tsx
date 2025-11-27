495db3dd74437c8fb141b9e78c82e0c513e9136d
farza <farzanrichu610@gmail.com>
2025-11-27 20:03:53 +0530

    fix default route

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Header() {
     return (
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-            <Link href="/public">Home</Link>
+            <Link href="/">Home</Link>
             <Link href="/about-us">About Us</Link>
             <Link href="/gallery">Gallery</Link>
             <Link href="/events">Events</Link>
