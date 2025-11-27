50ad7b8a7a1fbce77e1018e03c5096121abc7e05
farza <farzanrichu610@gmail.com>
2025-11-27 23:50:11 +0530

    component for footer links

---
+++ src/components/footerComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {ArrowUpRight} from "lucide-react";
+
+function FooterComponent({page}: {page: string}) {
+    return (
+        <div className="flex items-center gap-3  hover:text-blue-500 transition">
+            <span>{page}</span>
+            <ArrowUpRight size={14}/>
+        </div>
+    );
+}
+
+export default FooterComponent;
