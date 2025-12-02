3ac54b9dd07ab3996d15f900da47e1eaa0b7125d
dhjr <dhananjayrjs1605@gmail.com>
2025-12-02 22:50:45 +0530

    vercel commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "@/components/footer";
 import Navbar from "@/components/navBar";
 
 export const metadata: Metadata = {
-  title: "TechFuse 2.0",
+  title: "TechFuse 2.0 ",
   description: "An IEEE SPS RIT EVENT",
 };
 
