2c82eafd52ce52f848f4aa841bd6d841066c12ca
Farzan R S <farzanrichu610@gmail.com>
2025-12-04 23:58:10 +0530

    card styling

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import EventData from "@/data/eventData";
+import {Calendar, MapPin, Tag} from "lucide-react";
 
 
 interface EventCardProps {
@@ -10,14 +11,40 @@ interface EventCardProps {
 export default function EventCard({data}: EventCardProps) {
     return (
 
-        <div>
-            <Image
-                src={data.src}
-                alt="Event Image"
-                width={300}
-                height={200}
-                className="rounded-lg object-cover"
-            />
+        <div className="flex flex-col bg-gradient-to-br from-white/10 via-white/10 to-white/10 backdrop-blur-xl  rounded-t-xl rounded-b-xl max-w-sm mx-auto shadow-2xl ">
+            <div className="relative rounded-t-xl overflow-hidden">
+
+                <div
+                    className="absolute top-2 right-2 flex items-center gap-1 bg-white/30 backdrop-blur-md text-[10px] text-white px-2 py-1 rounded-full shadow">
+                    <span>{data.eventType}</span>
+                </div>
+
+                <Image
+                    src={data.src}
+                    alt="Event Image"
+                    width={300}
+                    height={200}
+                    className="rounded-t-xl object-cover"
+                />
+            </div>
+
+            <div>
+                <div
+                    className=" flex justify-around w-full items-center font-extrabold mb-3 leading-tight tracking-wide">
+                    <div className="flex items-center gap-2 text-sm opacity-80">
+                        <MapPin size={16}/>
+                        <span>{data.location}</span>
+                    </div>
+
+                    <div className="flex items-center gap-2 text-sm opacity-80">
+                        <Calendar size={16}/>
+                        <span>{data.date}</span>
+                    </div>
+                </div>
+                <div>
+                    {data.eventName}
+                </div>
+            </div>
         </div>
     )
 
