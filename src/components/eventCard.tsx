ba3d08ce2722981d60d6773a44e3add09088a1ab
Farzan R S <farzanrichu610@gmail.com>
2025-12-04 22:01:56 +0530

    eventCard.tsx work started

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -1,11 +1,23 @@
-import {useState} from "react";
+import Image from "next/image";
+import EventData from "@/data/eventData";
 
 
-export default function EventCard(){
+interface EventCardProps {
+    // @ts-ignore
+    data: EventData,
+}
+
+export default function EventCard({data}: EventCardProps) {
     return (
 
         <div>
-            
+            <Image
+                src={data.src}
+                alt="Event Image"
+                width={300}
+                height={200}
+                className="rounded-lg object-cover"
+            />
         </div>
     )
 
