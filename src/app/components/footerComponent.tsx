eb4d5b5402e9242ef6a6658fe76a802fb5ca3dca
dhjr <dhananjayrjs1605@gmail.com>
2025-12-05 01:15:25 +0530

    updated imports

--- src/app/components/footerComponent.tsx
+++ src/app/components/footerComponent.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import {ArrowUpRight} from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 
-function FooterComponent({page}: {page: string}) {
-    return (
-        <div className="flex items-center gap-3 text-white hover:text-[#66FFFF]  transition">
-            <span>{page}</span>
-            <ArrowUpRight size={14}/>
-        </div>
-    );
+function FooterComponent({ page }: { page: string }) {
+  return (
+    <div className="flex items-center gap-3 text-white hover:text-[#66FFFF]  transition">
+      <span>{page}</span>
+      <ArrowUpRight size={14} />
+    </div>
+  );
 }
 
 export default FooterComponent;
--- src/app/components/heading.tsx
+++ src/app/components/heading.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
-function Heading({text}: {text: string}) {
-    return (
-        <h1 className="font-sans lg:text-3xl text-xl text-center text-tf-accent font-semibold lg:max-w-[180px] max-w-[100px] mx-auto border-b-2 border-white/70 pb-1 mb-4">
-            {text}
-        </h1>
-    );
+function Heading({ text }: { text: string }) {
+  return (
+    <h1 className="font-sans lg:text-3xl text-xl text-center text-tf-accent font-semibold lg:max-w-[180px] max-w-[100px] mx-auto border-b-2 border-white/70 pb-1 mb-4">
+      {text}
+    </h1>
+  );
 }
 
 export default Heading;
