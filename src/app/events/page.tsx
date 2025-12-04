98bbcd8d8cd7e3843be9d237bc0d2f595a042631
Farzan R S <farzanrichu610@gmail.com>
2025-12-04 22:01:37 +0530

    basic structre set

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Heading from "@/components/heading";
+import EventCard from "@/components/eventCard";
+import eventData from "@/data/eventData";
 
 function events() {
     return (
-        <div>
-            <Heading text={"Events"}/>
+        <div className="p-4">
+            <Heading text="Events"/>
+
+            {eventData.map((event) => (
+                // @ts-ignore
+                <EventCard key={event.eventId} data={event} />
+            ))}
         </div>
     );
 }
