e9457668bbf2b26a7786f9e3179ef129d3ab1518
Farzan R S <farzanrichu610@gmail.com>
2025-12-06 17:28:20 +0530

    event modal almost done

--- src/app/components/eventModal.tsx
+++ src/app/components/eventModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Button from "@/app/components/button";
+import { X } from "lucide-react";
 
 interface Modal {
     src: string;
@@ -17,7 +18,17 @@ interface EventModalProps {
 export default function EventModal({data, onClose}: EventModalProps) {
     return (
         <div className="fixed inset-0 z-50 bg-black/40 backdrop-blur-md flex justify-center items-center p-4">
-            <div className="flex flex-row bg-red-500 gap-5 ">
+            <div className=" flex flex-row gap-5 w-[48rem] bg-white/10 backdrop-blur-xl border border-white/20 shadow-xl rounded-2xl p-6">
+                {onClose && (
+                    <button
+                        onClick={onClose}
+                        className="absolute top-4 right-4 w-9 h-9 flex items-center justify-center
+                               rounded-full  hover:bg-white/30 backdrop-blur-lg
+                               border border-white/30 shadow-lg transition cursor-pointer"
+                    >
+                        <X size={20} className="text-white" />
+                    </button>
+                )}
                 <Image
                     src={data.src}
                     alt={data.name}
@@ -36,11 +47,6 @@ export default function EventModal({data, onClose}: EventModalProps) {
                         <Button text="Get Tickets" link="#"/>
                     )}
 
-                    {onClose && (
-                        <button onClick={onClose} className="mt-4 p-2 bg-gray-200 rounded">
-                            Close
-                        </button>
-                    )}
                 </div>
             </div>
         </div>
