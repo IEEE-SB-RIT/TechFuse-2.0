8ce08405ffa1fe933ec9deafebd6e4fe5a7d7a40
Farzan R S <farzanrichu610@gmail.com>
2025-12-08 12:18:59 +0530

    implementaion of the ui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,26 @@
 import Heading from "@/app/components/heading";
+import AboutTemplate from "@/app/components/aboutTemplate";
+import aboutData from "@/app/lib/aboutData";
 
 function AboutUs() {
-  return (
-    <div>
-      <Heading text={"About Us"} />
-    </div>
-  );
+    return (
+        <div>
+            <Heading text={"About Us"}/>
+            <div className="flex flex-col gap-6">
+                {aboutData.map((data, index) => (
+                    <AboutTemplate
+                        key={index}
+                        // @ts-ignore
+                        heading={data.heading}
+                        src={data.src}
+                        description={data.description}
+                        align={index % 2 === 0 ? "flex-row" : "flex-row-reverse"}
+                    />
+                ))}
+            </div>
+
+        </div>
+    );
 }
 
 export default AboutUs;
