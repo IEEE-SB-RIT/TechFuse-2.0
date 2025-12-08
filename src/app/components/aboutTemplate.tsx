d2e49d8b101126cbcf953a44a42b28deab6d6159
Farzan R S <farzanrichu610@gmail.com>
2025-12-08 12:28:56 +0530

    work ongoing

--- src/app/components/aboutTemplate.tsx
+++ src/app/components/aboutTemplate.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import Image from "next/image";
+import SubHeading from "@/app/components/subHeading";
 
-interface Data{
-    heading:string;
-    src:string;
-    description:string;
-    align?:string;
+interface Data {
+    heading: string;
+    src: string;
+    description: string;
+    align?: string;
 }
 
-function AboutTemplate(Data:Data) {
+function AboutTemplate(Data: Data) {
     return (
         <div className={`flex ${Data.align} items-center gap-6 justify-center`}>
-            {Data.heading}
-            <Image
-            src={Data.src}
-            width={300}
-            height={300}
-            alt="About Image"
-            className="rounded-lg object-cover "
-            />
-            <p>
-                {Data.description}
-            </p>
+            <div className="flex flex-col items-start justify-center gap-4">
+                <SubHeading title={Data.heading}/>
+
+                <Image
+                    src={Data.src}
+                    width={300}
+                    height={300}
+                    alt="About Image"
+                    className="rounded-lg object-cover"
+                />
+            </div>
+
+            <p className="max-w-[400px] leading-relaxed">{Data.description}</p>
         </div>
     );
 }
