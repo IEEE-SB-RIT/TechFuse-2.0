858e306ec51bb4d25cc4052b0d81a941515d6b23
Farzan R S <farzanrichu610@gmail.com>
2025-12-08 12:18:41 +0530

    some data for usability

---
+++ src/app/lib/aboutData.ts
@@ -0,0 +1,24 @@
+interface aboutData {
+    heading?: string;
+    src: string;
+    description: string;
+}
+
+const aboutData: aboutData[] = [
+  {
+      heading: "Tech Fuse 2.0",
+    src: "/about-us-image-1.webp",
+    description: "Company history and mission."
+  },
+  {
+      heading: "IEEE SB RIT ",
+    src: "/about-us-image-2.webp",
+    description: "Our team and culture."
+  },
+  {
+      heading: "IEEE SPS RIT",
+    src: "/about-us-image-3.webp",
+    description: "What we build and why it matters."
+  }
+];
+export default aboutData;
