5ad86f49725c0c40004f276da7b8ad7e8a845014
Farzan R S <farzanrichu610@gmail.com>
2025-12-09 12:09:27 +0530

    update aboutData.ts

--- src/app/lib/aboutData.ts
+++ src/app/lib/aboutData.ts
@@ -1,5 +1,3 @@
-import {lorem} from "next/dist/next-devtools/dev-overlay/utils/lorem";
-
 interface aboutData {
     heading?: string;
     src: string;
@@ -7,23 +5,30 @@ interface aboutData {
 }
 
 const aboutData: aboutData[] = [
-  {
-      heading: "Tech Fuse 2.0",
-    src: "/about-us-image-1.webp",
-      description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi, delectus eaque esse eum excepturi impedit maiores maxime nobis numquam odio odit officiis quas quod reprehenderit totam unde? Architecto, reprehenderit"
-
-  },
-  {
-      heading: "IEEE SB RIT ",
-    src: "/about-us-image-2.webp",
-      description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi, delectus eaque esse eum excepturi impedit maiores maxime nobis numquam odio odit officiis quas quod reprehenderit totam unde? Architecto, reprehenderit"
-
-  },
-  {
-      heading: "IEEE SPS RIT",
-    src: "/about-us-image-3.webp",
-      description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi, delectus eaque esse eum excepturi impedit maiores maxime nobis numquam odio odit officiis quas quod reprehenderit totam unde? Architecto, reprehenderit"
-
-  }
+    {
+        heading: "TechFuse 2.0",
+        src: "/about-us-image-1.webp",
+        description:
+            "Join us for TechFuse 2.0, the ultimate three-day event organized by IEEE SPS SBC RIT in partnership with IEEE SPS Kerala Chapter hosted at Rajiv Gandhi Institute of Technology, Kottayam! Dive into hands-on workshops, interactive sessions, and expert talks focused on cutting-edge signal processing and technology. Unleash your creativity through hands-on learning in Edge AI and IoT, Virtual Reality, and ADAS using embedded systems. TechFuse 2.0 is your ticket to innovation and collaboration—let's inspire the engineer in you to explore, create, and excel! Don’t miss out!"
+    },
+    {
+        heading: "IEEE SB RIT",
+        src: "/about-us-image-2.webp",
+        description:
+            "The IEEE SB RIT is one of the most vibrant and significant student branches in the Kerala section. Established in January 2006, the IEEE student branch of Rajiv Gandhi Institute of Technology Kottayam has proven to be a powerhouse of highly dedicated and energetic volunteers."
+    },
+    {
+        heading: "IEEE SPS SBC RIT",
+        src: "/about-us-image-3.webp",
+        description:
+            "Founded on February 2, 2022, IEEE SPS SBC RIT has quickly risen as a standout chapter in the IEEE Kerala Section. Within just three years, it has become one of the most active SBCs, hosting impactful events that bridge theory and real-world signal processing applications, received multiple grants, reflecting its dedication to excellence and community impact."
+    },
+    {
+        heading: "IEEE SPS Kerala Chapter",
+        src: "/about-us-image-4.webp",
+        description:
+            "The IEEE Signal Processing Society (SPS) Kerala Chapter is an exciting community of 23 student leaders and 20 industry professionals dedicated to transforming signal processing! With support from a dynamic advisory body, they organize impactful events and empower SPS branches across Kerala, contributing to prestigious events like ICIP and ICASSP. Their vibrant collaborations with other IEEE chapters create a global network that sparks creativity and knowledge-sharing in this ever-evolving field!"
+    }
 ];
+
 export default aboutData;
