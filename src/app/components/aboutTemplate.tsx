f0543e5c7589e043ee54e5a50e46371d2a7bd837
Farzan R S <farzanrichu610@gmail.com>
2025-12-10 01:00:35 +0530

    fix logo alignemnt

--- src/app/components/aboutTemplate.tsx
+++ src/app/components/aboutTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Image from "next/image";
 import SubHeading from "@/app/components/subHeading";
 
@@ -8,59 +8,62 @@ interface Data {
   description: string;
   align?: "flex-row" | "flex-row-reverse";
   animation?: { className: string; style?: React.CSSProperties };
+  className?: string;
 }
 
 function AboutTemplate(Data: Data) {
+
   const layout = Data.align || "flex-row";
 
   return (
-    <div className="flex flex-col items-center w-full py-8 px-6">
-      <div
-        className={`w-full text-left px-5 md:px-8 py-2 ${Data.animation?.className}`}
-        style={Data.animation?.style}
-      >
-        <SubHeading title={Data.heading} />
-      </div>
+      <div className="flex flex-col items-center w-full py-8 px-6">
 
-      <div
-        className={`flex ${layout} flex-col-reverse md:${layout} 
+
+        <div
+            className={`w-full text-left px-5 md:px-8 py-2 ${Data.animation?.className}`}
+            style={Data.animation?.style}
+        >
+          <SubHeading title={Data.heading}/>
+        </div>
+
+        <div className={`flex ${layout} flex-col-reverse md:${layout} 
                 items-center justify-between gap-10 md:gap-14 
-                w-full py-12 px-4 md:px-8`}
-      >
-        <div
-          className={`max-w-[550px] space-y-4 min-h-[220px] ${
-            layout === "flex-row-reverse" ? "md:pl-10" : "md:pr-10"
-          } ${Data.animation?.className}`}
-          style={Data.animation?.style}
-        >
-          <p
-            className="font-sans text-lg md:text-xl font-medium leading-relaxed
-                        text-gray-700 dark:text-gray-300 tracking-wide"
-          >
-            {Data.description}
-          </p>
-        </div>
+                w-full py-12 px-4 md:px-8`}>
 
-        <div
-          className={`flex justify-center ${Data.animation?.className}`}
-          style={Data.animation?.style}
-        >
+
           <div
-            className="relative overflow-hidden rounded-2xl shadow-lg
-                        hover:shadow-2xl transition-all duration-300
-                        hover:scale-[1.02]"
+              className={`max-w-[550px] space-y-4 min-h-[220px] ${
+                  layout === "flex-row-reverse" ? "md:pl-10" : "md:pr-10"
+              } ${Data.animation?.className}`}
+              style={Data.animation?.style}
           >
-            <Image
-              src={Data.src}
-              width={520}
-              height={320}
-              alt={Data.heading}
-              className="rounded-2xl object-cover h-[320px] w-[520px]"
-            />
+            <p className="font-sans text-lg md:text-xl font-medium leading-relaxed
+                        text-gray-700 dark:text-gray-300 tracking-wide">
+              {Data.description}
+            </p>
           </div>
+
+          <div
+              className={`flex justify-center ${Data.animation?.className}`}
+              style={Data.animation?.style}
+          >
+            <div className="relative overflow-hidden rounded-2xl shadow-lg
+                        hover:shadow-2xl transition-all duration-300
+                        hover:scale-[1.02]">
+
+              <Image
+                  src={Data.src}
+                  width={520}
+                  height={320}
+                  alt={Data.heading}
+                  className={`w-[520px] h-[320px] ${Data.className || "object-cover"}`}
+              />
+
+            </div>
+          </div>
+
         </div>
       </div>
-    </div>
   );
 }
 
--- src/app/lib/aboutData.ts
+++ src/app/lib/aboutData.ts
@@ -2,14 +2,16 @@ interface aboutData {
     heading?: string;
     src: string;
     description: string;
+    className?: string;
 }
 
 const aboutData: aboutData[] = [
     {
         heading: "TechFuse 2.0",
-        src: "/about-us-image-1.webp",
+        src: "/techfuselogo.webp",
         description:
-            "Join us for TechFuse 2.0, the ultimate three-day event organized by IEEE SPS SBC RIT in partnership with IEEE SPS Kerala Chapter hosted at Rajiv Gandhi Institute of Technology, Kottayam! Dive into hands-on workshops, interactive sessions, and expert talks focused on cutting-edge signal processing and technology. Unleash your creativity through hands-on learning in Edge AI and IoT, Virtual Reality, and ADAS using embedded systems. TechFuse 2.0 is your ticket to innovation and collaboration—let's inspire the engineer in you to explore, create, and excel! Don’t miss out!"
+            "Join us for TechFuse 2.0, the ultimate three-day event organized by IEEE SPS SBC RIT in partnership with IEEE SPS Kerala Chapter hosted at Rajiv Gandhi Institute of Technology, Kottayam! Dive into hands-on workshops, interactive sessions, and expert talks focused on cutting-edge signal processing and technology. Unleash your creativity through hands-on learning in Edge AI and IoT, Virtual Reality, and ADAS using embedded systems. TechFuse 2.0 is your ticket to innovation and collaboration—let's inspire the engineer in you to explore, create, and excel! Don’t miss out!",
+        className: "object-contain w-[520px] h-[320px] bg-tf-radial-pattern"
     },
     {
         heading: "IEEE SB RIT",
